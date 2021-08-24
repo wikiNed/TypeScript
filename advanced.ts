823b683b80c71c5d2d27aca6c98d1f53d3d591a3
wikiNed <1007421637@qq.com>
2021-08-25 00:26:49 +0800

    feat（添加泛型示例）

--- advanced.ts
+++ advanced.ts
@@ -187,3 +187,14 @@ function createArray<T>(length:number,value:T):Array<T>{
     }
     return result;
 }
+
+function copyFields<T extends U, U>(target: T, source: U): T {
+    for (let id in source) {
+        target[id] = (<T>source)[id];
+    }
+    return target;
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+copyFields(x, { b: 10, d: 20 });
